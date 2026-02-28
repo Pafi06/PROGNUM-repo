9482a77897ebc793df1c92b84b133f4a8c4d0771
Pafi06 <121343132+Pafi06@users.noreply.github.com>
2026-02-28 16:25:28 +0100

    Week 2 correction

--- Task2/JD.py
+++ Task2/JD.py
@@ -8,16 +8,21 @@ import math
 
 def jd(y,m,d):
     t1 = 367 * y
-    t2 = (7 * (y + (m + 9) // 12)) // 4
-    t3 = (3 * ((y + (m - 9) // 7) // 100 +1)) // 4
-    t4 = (275 * m) // 9
+    t2 = (7 * (y + (m + 9) / 12)) / 4
+    t3 = (3 * ((y + (m - 9) / 7) / 100 +1)) / 4
+    t4 = (275 * m) / 9
     jda = t1 - t2 - t3 + t4 + d + 1721029 - 0.5
     return jda
 
-y = int(input("Year:"))
-m = int(input("Month:"))
-d = int(input("Day:"))
+y1 = float(input("Year 1:"))
+m1 = float(input("Month 1:"))
+d1 = float(input("Day 1:"))
 
-date = jd(y,m,d)
-print(f"Julian date: {date}" )
+y2 = float(input("Year 2:"))
+m2 = float(input("Month 2:"))
+d2 = float(input("Day 2:"))
 
+date1 = jd(y1,m1,d1)
+date2 = jd(y2,m2,d2)
+
+print(f"Days difference: {date1-date2}" )
